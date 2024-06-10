5865e6c043bdc06122730a33798813f45c6a58f7
satrio.bayu <satrio.bayu@koinworks.com>
2024-06-10 09:59:28 +0700

    form page

--- src/routes/form.jsx
+++ src/routes/form.jsx
@@ -7,19 +7,16 @@ import { convertJsonSchema, convertUiSchema } from "../adaptor";
 import data from "../data.json";
 import modelForm from "../data-oss-2.json";
 import { createFileRoute } from "@tanstack/react-router";
+import { Button, Select } from "@mantine/core";
+import { useMemo } from "react";
 
 function FormPage() {
   const [allFormData, setAllFormData] = useState(null);
-  const [selectedForm, setSelectedForm] = useState({ name: null, id: null });
-  const [isGetFormDataClicked, setIsGetFormDataClicked] = useState(false);
   const [isConvertForm, setIsConvertForm] = useState(false);
   const [schema, setSchema] = useState({});
   const [uiSchema, setUiSchema] = useState({});
   function getData() {
     setAllFormData(data);
-  }
-  function selectForm(value) {
-    setSelectedForm(value);
   }
 
   function convertDataSchema() {
@@ -30,81 +27,53 @@ function FormPage() {
 
   const log = (type) => console.log.bind(console, type);
 
+  // console.log(allFormData);
+
+  const formDatas = useMemo(
+    () =>
+      data.data.map((item) => ({
+        value: item.id,
+        label: item.name,
+        key: item.id,
+      })),
+    [data]
+  );
+
   return (
     <div className="p-8">
       <div className="mb-8">
         <span>Get All Forms</span>
-        <button
-          className="ml-4 p-2 border border-black bg-slate-300"
-          onClick={() => getData()}
-        >
-          Klik here
-        </button>
+        <Button className="ml-4" onClick={() => getData()}>
+          Click here
+        </Button>
       </div>
       <div className="mb-8 border border-black p-4">
         <p className="mb-4 text-2xl">List all forms</p>
-        <ul>
-          {allFormData === null ? (
-            <li>null</li>
-          ) : (
-            allFormData.data.map((item) => (
-              <li
-                key={item.id}
-                className="mb-4 hover:bg-blue-400"
-                onClick={() => selectForm({ name: item.name, id: item.id })}
-              >
-                Name: {item.name}
-                <ul>
-                  <li>Key: {item.key}</li>
-                  <li>Id: {item.id}</li>
-                  <li>Version: {item.version}</li>
-                </ul>
-              </li>
-            ))
-          )}
-        </ul>
-      </div>
-      <p className="mb-4">
-        Selected form:{" "}
-        <span>{`${selectedForm.name} --- ${selectedForm.id}`}</span>
-      </p>
-      <p className="mb-4">
-        Get selected form model{" "}
-        <button
-          className="ml-4 p-2 border border-black bg-slate-300"
-          onClick={() => setIsGetFormDataClicked(!isGetFormDataClicked)}
-        >
-          Klik Here
-        </button>
-      </p>
-      <p className="mb-4">Form model</p>
-      <div className="mb-8 border border-black p-4">
-        {isGetFormDataClicked ? (
-          <pre>{JSON.stringify(modelForm, null, 2)}</pre>
+        {allFormData === null ? (
+          <span>no Data...</span>
         ) : (
-          <>No Data...</>
+          <Select data={formDatas} />
         )}
       </div>
-      <p className="mb-8">
-        Convert form model into RJSF{" "}
-        <button
-          className="ml-4 p-2 border border-black bg-slate-300"
-          onClick={() => convertDataSchema()}
-        >
-          Klik Here
-        </button>
+      <p className="mb-4">
+        Get selected form model and Convert Form{" "}
+        <Button className="ml-4" onClick={() => convertDataSchema()}>
+          Click here
+        </Button>
       </p>
       <p className="mb-4">RJSF FORMS</p>
-      {isConvertForm ? (
-        <Form
-          schema={schema}
-          validator={validator}
-          uiSchema={uiSchema}
-          onChange={log("changed")}
-          onSubmit={log("submitted")}
-          onError={log("errors")}
-        />
-      ) : null}
+      <div className="border border-slate-500 p-4">
+        {isConvertForm ? (
+          <Form
+            schema={schema}
+            validator={validator}
+            uiSchema={uiSchema}
+            onChange={log("changed")}
+            onSubmit={log("submitted")}
+            onError={log("errors")}
+          />
+        ) : null}
+      </div>
     </div>
   );
 }
