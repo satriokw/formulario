917953aa53fda9b2826a6b4f494a03877f387f1c
satrio.bayu <satrio.bayu@koinworks.com>
2024-06-11 23:34:12 +0700

    update adaptor: skip convert non input tag

--- src/adaptor.js
+++ src/adaptor.js
@@ -14,6 +14,7 @@ const typeMap = {
 
 // function JSONSchema convert
 export function convertJsonSchema(data) {
+  // function JSONSchema convert
   const transformedData = {
     title: data.name,
     description: "",
@@ -23,66 +24,69 @@ export function convertJsonSchema(data) {
   };
   data.fields.forEach((field) => {
     const { id, name, type } = field;
-    const property = {
-      title: name,
-      type: typeMap[type],
-    };
 
-    // min-max length
-    if (field.hasOwnProperty("params") && field.params.minLength) {
-      property.minLength = Number.parseInt(field.params.minLength);
-    }
-    if (field.hasOwnProperty("params") && field.params.maxLength) {
-      property.maxLength = Number.parseInt(field.params.maxLength);
-    }
+    // if the type is not on the typeMap, it will skip it.
+    if (type in typeMap) {
+      const property = {
+        title: name,
+        type: typeMap[type],
+      };
 
-    // Check if either the id or the name contains the string "email" to format email
-    if (
-      field.id.toLowerCase().includes("email") ||
-      field.name.toLowerCase().includes("email")
-    ) {
-      // Set the variable property_format to "email"
-      property.format = "email";
-    }
+      // min-max length
+      if (field.hasOwnProperty("params") && field.params.minLength) {
+        property.minLength = Number.parseInt(field.params.minLength);
+      }
+      if (field.hasOwnProperty("params") && field.params.maxLength) {
+        property.maxLength = Number.parseInt(field.params.maxLength);
+      }
 
-    // pattern
-    if (field.hasOwnProperty("params") && field.params.regexPattern) {
-      property.pattern = `/^${field.params.regexPattern}$/`;
-    }
+      // Check if either the id or the name contains the string "email" to format email
+      if (
+        field.id.toLowerCase().includes("email") ||
+        field.name.toLowerCase().includes("email")
+      ) {
+        // Set the variable property_format to "email"
+        property.format = "email";
+      }
 
-    // radio-button and dropdown
-    if (type === "dropdown" || type === "radio-buttons") {
-      property.enum = field.options.map((item) => item.name);
-    }
+      // pattern
+      if (field.hasOwnProperty("params") && field.params.regexPattern) {
+        property.pattern = `/^${field.params.regexPattern}$/`;
+      }
 
-    // upload
-    if (type === "upload") {
-      property.format = "data-url";
-    }
+      // radio-button and dropdown
+      if (type === "dropdown" || type === "radio-buttons") {
+        property.enum = field.options.map((item) => item.name);
+      }
 
-    // date
-    if (type === "date") {
-      property.format = "date";
-    }
+      // upload
+      if (type === "upload") {
+        property.format = "data-url";
+      }
 
-    // default value
-    if (field.hasOwnProperty("value") && field.value !== null) {
-      property.default = field.value;
-    }
-    if (
-      field.type === "boolean" &&
-      field.hasOwnProperty("value") &&
-      field.value === null
-    ) {
-      property.default = false;
-    }
+      // date
+      if (type === "date") {
+        property.format = "date";
+      }
 
-    // required form
-    if (field.required) {
-      transformedData.required.push(id);
-    }
+      // default value
+      if (field.hasOwnProperty("value") && field.value !== null) {
+        property.default = field.value;
+      }
+      if (
+        field.type === "boolean" &&
+        field.hasOwnProperty("value") &&
+        field.value === null
+      ) {
+        property.default = false;
+      }
 
-    transformedData.properties[id] = property;
+      // required form
+      if (field.required) {
+        transformedData.required.push(id);
+      }
+      transformedData.properties[id] = property;
+    }
   });
 
   return transformedData;
@@ -90,34 +94,36 @@ export function convertJsonSchema(data) {
 
 // function UISchema convert
 export function convertUiSchema(data) {
-  const uiSchema = {};
-  data.fields.forEach((field) => {
-    const { id } = field;
-    const uiProperty = {};
+  const { id } = field;
+
+  // if the type is not on the typeMap, it will skip it.
+  if (type in typeMap) {
+    const uiProperty = {}
 
     // placeholder
     if (field.placeholder) {
-      uiProperty["ui:placeholder"] = field.placeholder;
+      uiProperty["ui:placeholder"] = field.placeholder
     }
     // readonly
     if (field.readOnly) {
-      uiProperty["ui:readonly"] = field.readOnly;
+      uiProperty["ui:readonly"] = field.readOnly
     }
     // password
-    if (field.type === "password") {
-      uiProperty["ui:widget"] = "password";
+    if (field.type === 'password') {
+      uiProperty["ui:widget"] = 'password'
     }
 
     // radio-buttons
-    if (field.type === "radio-buttons") {
-      (uiProperty["ui:widget"] = "radio"),
-        (uiProperty["ui:options"] = {
-          inilne: true,
-        });
+    if (field.type === 'radio-buttons'
+    ) {
+      uiProperty["ui:widget"] = "radio",
+      uiProperty["ui:options"] = {
+        "inilne": true
+      }
     }
 
-    uiSchema[id] = uiProperty;
-  });
+    uiSchema[id] = uiProperty
+  }
 
   return uiSchema;
 }
